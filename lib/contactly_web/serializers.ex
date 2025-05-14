f7b00ead645ccbedabb0564dcc6b61323dd297d1
joangavelan <joangavelan@gmail.com>
2025-05-14 00:05:10 -0500

    refactor: use Map.take/2 for serialization

--- lib/contactly_web/serializers.ex
+++ lib/contactly_web/serializers.ex
@@ -1,22 +1,12 @@
 defmodule ContactlyWeb.Serializers do
   def serialize_user(user) when is_map(user) do
-    %{
-      id: user.id,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      email: user.email
-    }
+    Map.take(user, [:id, :first_name, :last_name, :email])
   end
 
   def serialize_user(_), do: nil
 
   def serialize_contact(contact) when is_map(contact) do
-    %{
-      id: contact.id,
-      name: contact.name,
-      email: contact.email,
-      phone: contact.phone
-    }
+    Map.take(contact, [:id, :name, :email, :phone])
   end
 
   def serialize_contacts(contacts) when is_list(contacts) do
